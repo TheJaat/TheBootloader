ca03c87aa0b993d3c046fd897fce81d1114f62c1
The Jat <manishkl543@gmail.com>
2025-01-18 00:21:17 +0530

    Implement Print_String16 function for printing in real mode using interrupts

--- src/boot/stage1.asm
+++ src/boot/stage1.asm
@@ -1,16 +1,23 @@
 BITS 16                 ; Set the code to 16-bit mode (required for bootloaders)
 ORG 0x7c00              ; Set the origin to 0x7C00, where the BIOS loads the bootloader
 
+jmp main_gate
+
+%include "../../headers/boot/print16.inc"
+
 ; Entry point of the bootloader
 main_gate:
-    ; Print the character 'J' to the screen
-    mov ah, 0x0e        ; BIOS teletype function (interrupt 0x10, AH=0x0E)
-    mov al, 'J'         ; Load the ASCII value of 'J' into AL
-    int 0x10            ; Call the BIOS interrupt to print the character
+
+    mov si, welcome_string
+    call Print_String16
 
     ; Infinite loop to prevent execution from continuing into unknown memory
 hang:
     jmp hang            ; Loop forever
+
+
+; data section
+welcome_string: db "Welcome to stage 1", 0
 
 ; Padding to ensure the bootloader is 512 bytes (required by BIOS)
 padding:
@@ -18,4 +25,3 @@ padding:
 
 ; Boot signature (mandatory for BIOS to recognize this as a valid bootloader)
 boot_signature dw 0xAA55 ; Signature value at the end of the 512-byte sector
-
