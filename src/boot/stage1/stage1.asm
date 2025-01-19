38a3100134ef778854a5f27cd1eaa15a479f5f11
The Jat <manishkl543@gmail.com>
2025-01-19 16:42:23 +0530

    Add function to clear the screen and reset the cursor to default top left position.

--- src/boot/stage1/stage1.asm
+++ src/boot/stage1/stage1.asm
@@ -1,12 +1,51 @@
 BITS 16                 ; Set the code to 16-bit mode (required for bootloaders)
 ORG 0x7c00              ; Set the origin to 0x7C00, where the BIOS loads the bootloader
 
-jmp main_gate
+main_gate:
 
+jmp main
+
+;; Include files
 %include "utils16/print16.inc"
 
 ; Entry point of the bootloader
-main_gate:
+main:
+    ; Disable Interrupts
+    cli
+
+    ; Far jump to fix segment registers
+    jmp 0x0:FixCS
+
+FixCS:
+    ; Fix segment registers to 0
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+
+    ;; ******************************************
+    ;; Set Stack
+    ;; The stack segment (SS) and stack pointer (SP) are initialized to 
+    ;; ensure the stack operates correctly. The stack grows downward in memory.
+    ;; 
+    ;; SS is set to the same value as the code segment (CS) because, in a 
+    ;; bootloader, code and stack often reside in the same segment. 
+    ;; SP is set to 0x7C00, which is just below the start of the bootloader's 
+    ;; code (0x7C00 is the entry point for the boot sector). This setup ensures 
+    ;; there is space for the stack to grow downward without interfering with 
+    ;; other memory regions.
+    ;; AX is 0x0000 (segment base address for SS)
+    mov ss, ax            ; Set the Stack Segment (SS)
+    mov sp, 0x7C00        ; Set the Stack Pointer (SP) to 0x7C00
+    ;; ******************************************
+
+    ; Set interrupts
+    sti
+
+    ; Save the DL register value, which contains the disk number
+    mov byte [bPhysicalDriveNum], dl
+
+    call ClearScreenAndResetCursor    ; Clear the screen and reset the
+                                      ; cursor at the top left corner
 
     ;; Print the stage1 welcome message
     mov si, welcome_stage1_str
@@ -19,8 +58,12 @@ hang:
 
 
 
+;; ******************************************
+;; data section
+;; ******************************************
 
-; data section
+bPhysicalDriveNum db 0	; Variable to store disk number
+
 welcome_stage1_str: db "Welcome to stage 1", 0
 
 
