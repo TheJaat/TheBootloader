f471cad896595270216030be6e5860855306bf99
The Jat <manishkl543@gmail.com>
2025-02-02 17:32:10 +0530

    Generate listing file for stage1.asm which contains the line number macro expansion, symbol tables, memory address, machine code and source code

--- headers/revision.h
+++ headers/revision.h
@@ -4,11 +4,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "02 February 2025"
-#define BUILD_TIME "11:52:26"
+#define BUILD_TIME "17:28:13"
 
 #define BUILD_SYSTEM "gcc"
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 1
-#define REVISION_BUILD 16
+#define REVISION_BUILD 23
 
 #endif //!_REVISION_H_
