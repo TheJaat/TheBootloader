3b1696af4196b674eeda32a3d8a4ef85991fdcbd
The-Jat <manishkl543@gmail.com>
2025-07-19 15:36:04 +0000

    Bump version

--- headers/revision.h
+++ headers/revision.h
@@ -3,12 +3,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "21 February 2025"
-#define BUILD_TIME "22:09:05"
+#define BUILD_DATE "19 July 2025"
+#define BUILD_TIME "15:22:36"
 
 #define BUILD_SYSTEM "gcc"
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 2
-#define REVISION_BUILD 369
+#define REVISION_BUILD 377
 
 #endif //!_REVISION_H_
